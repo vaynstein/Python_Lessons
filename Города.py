d1ea9a2c2a7098d75bb43de20598ca2d9adbb701
Dmitry <gmyrak@gmail.com>
2018-10-23 21:13:14 +0300

    Добавден словарь стран.

--- Города.py
+++ Города.py
@@ -1,3 +1,13 @@
+r=open('base/country.csv',mode='r',encoding='cp1251')
+D={}
+for line in r:
+    line=line.strip()
+    R=line.split(';')
+    country_id=R[0].strip('"')
+    name=R[2].strip('"')
+    D[country_id]=name
+    print(country_id,name)
+
 f=open('base/city.csv', mode='r',encoding='cp1251')
 for line in f:
     #print(line,end='')
@@ -6,7 +16,6 @@ for line in f:
     country_id = C[1].strip('"')
     region_id=C[2].strip('"')
     name=C[3].strip('"')
-    print('Город {} находится в стране {} в регионе {}'.format(name,country_id,region_id))
-
+    print('Город {} находится в стране {} в регионе {}'.format(name,D[country_id],region_id))
 
 
