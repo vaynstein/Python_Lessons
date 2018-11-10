035af1896a28f8d19aab9d9e4db432e5fa3c1414
Dmitry <gmyrak@gmail.com>
2018-11-10 09:54:48 +0200

    new

--- Графика.py
+++ Графика.py
@@ -1,19 +1,21 @@
 from tkinter import *
-root=Tk()
+
+root = Tk()
 root.geometry('300x200')
 print('hello')
 root.title('графика')
 
+
 def button():
-   ent.get()
+    lab.config(text=ent.get())
+    print(ent.get())
 
 
-but=Button(root,text='ок', command= button)
+but = Button(root, text='ок', command=button)
 but.pack()
-Lab=Label(root, text='нажмите на кнопку')
-Lab.pack()
-ent=Entry(root)
+lab = Label(root, text='нажмите на кнопку')
+lab.pack()
+ent = Entry(root)
 ent.pack()
-print(but)
-#but.pack(side='left')
+
 root.mainloop()
--- последовательность.py
+++ последовательность.py
@@ -1,6 +1,6 @@
 def function(n):
-    x=n
-    i=0
+    x = n
+    i = 0
     while x>1:
         if x%2==0:
             x //= 2
@@ -9,7 +9,18 @@ def function(n):
         i += 1
     return i
 
+Max = 0
+Nm = 0
+
 
 for k in range(1,1000):
-    print(k,function(k))
+    res = function(k)
+    if res > Max:
+        Max = res
+        Nm = k
 
+print(Nm, Max)
+
+
+
+#print( "{1} {0}".format( *max((function(k), k) for k in range(1, 1000))))
