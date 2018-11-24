f7c503e02218a94b866de1413c451ce6f99a4036
Dmitry <gmyrak@gmail.com>
2018-11-24 13:12:09 +0200

    new

--- Эратосфен.py
+++ Эратосфен.py
@@ -1,13 +1,18 @@
 N = 1000
-A = [True]*(N+1)
+A = [True]*N
 
-for i in range(2, int(N**(1/2)+1)):
+i = 2
+m = N**(1/2)
+while i < m:
+    while not A[i]:
+        i += 1
     k = 2*i
-    while k <= N:
+    while k < N:
         A[k] = False
         k +=i
+    i += 1
 
-for k in range(2, N+1):
+for k in range(2, N):
     if A[k]:
         print(k)
 
